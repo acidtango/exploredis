dd86b85deb2778c49ae99fee38a3c4e5e5092647
rubenmoya <rmoyarodriguez@gmail.com>
2018-10-24 19:36:33 +0200

    ✅ Add specs for <Button />

---
+++ src/components/__tests__/Button.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils'
+import Button from '@/components/Button.vue'
+
+describe('Button.vue', () => {
+  it('renders correctly', () => {
+    const label = 'Kenobi'
+    const wrapper = shallowMount(Button, {
+      propsData: { label }
+    })
+    expect(wrapper.text()).toMatch(label)
+  })
+})
