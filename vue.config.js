d6f0e5aad7f69caf23610ebc6cba8833db29d1f0
rubenmoya <rmoyarodriguez@gmail.com>
2018-10-26 09:09:53 +0200

    Separate background and renderer folders

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
+const path = require("path");
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pages: {
+    index: {
+      entry: "src/renderer/main.js",
+      template: "public/index.html",
+      filename: "index.html"
+    }
+  },
+  pluginOptions: {
+    electronBuilder: {
+      mainProcessFile: "src/background/main.js"
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src/renderer")
+      }
+    }
+  }
 };
