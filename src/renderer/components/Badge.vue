e93e0bc5fa136e6677446971b62926881950d919
Ruben Moya <ruben@acidtango.com>
2018-10-26 18:38:52 +0200

    WIP

---
+++ src/renderer/components/Badge.vue
@@ -0,0 +1,38 @@
+<template>
+  <span>
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Badge',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" scoped>
+span {
+  align-items: center;
+  background: #8983be;
+  border-radius: 2px;
+  color: white;
+  display: flex;
+  font-family: 'Overpass Mono', monospace;
+  font-size: 12px;
+  font-weight: 600;
+  height: 18px;
+  justify-content: center;
+  width: 32px;
+}
+</style>
+
+<docs>
+This badge is amazing, use it responsibly.
+
+## Examples
+
+```jsx
+<Badge>String</Badge>
+```
+</docs>
---
+++ src/renderer/views/Database/components/KeysList.vue
@@ -0,0 +1,63 @@
+<template>
+  <ul>
+    <li :key="key.name" v-for="key in keys">
+      <badge>{{type(key.type)}}</badge>
+      <span class="name">{{key.name}}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+import Badge from '@/components/Badge'
+
+export default {
+  name: 'KeysList',
+  components: { Badge },
+  props: {
+    keys: {
+      type: Array,
+      default: [],
+    },
+  },
+  methods: {
+    type(type) {
+      return type === 'string' ? 'str' : type
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+ul {
+  background-color: #535f6e;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  overflow: auto;
+  width: 250px;
+}
+
+li {
+  list-style: none;
+  padding: 8px 12px;
+  color: white;
+  font-family: 'Overpass Mono', monospace;
+  font-size: 12px;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+
+  .name {
+    margin-left: 12px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    width: 100%;
+  }
+
+  &:nth-child(even) {
+    background-color: rgba(69, 82, 98, 0.35);
+  }
+}
+</style>
+
