2ad55794ac0c2a7bc9517aaf29085bd445808451
rubenmoya <rmoyarodriguez@gmail.com>
2018-11-01 12:32:05 +0100

    ✅ Test <Button /> with snapshots

--- src/renderer/components/__tests__/Button.spec.js
+++ src/renderer/components/__tests__/Button.spec.js
@@ -1,12 +1,51 @@
 import { shallowMount } from '@vue/test-utils'
 import Button from '@/components/Button.vue'
 
-describe('Button.vue', () => {
-  it('renders correctly', () => {
-    const label = 'Kenobi'
-    const wrapper = shallowMount(Button, {
-      propsData: { label },
-    })
-    expect(wrapper.text()).toMatch(label)
+describe('<Button />', () => {
+  it('renders a label', () => {
+    const propsData = {
+      label: 'Name',
+    }
+    const wrapper = shallowMount(Button, { propsData })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders a placeholder', () => {
+    const propsData = {
+      label: 'Name',
+      placeholder: 'Name',
+    }
+    const wrapper = shallowMount(Button, { propsData })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders with disabled state', () => {
+    const propsData = {
+      label: 'Name',
+      placeholder: 'Anakin Skywalker',
+      disabled: true,
+    }
+    const wrapper = shallowMount(Button, { propsData })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders with readOnly state', () => {
+    const propsData = {
+      label: 'Name',
+      placeholder: 'Name',
+      readOnly: true,
+    }
+    const wrapper = shallowMount(Button, { propsData })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders with an initial value', () => {
+    const propsData = {
+      label: 'Name',
+      placeholder: 'Name',
+      value: 'General Kenobi',
+    }
+    const wrapper = shallowMount(Button, { propsData })
+    expect(wrapper).toMatchSnapshot()
   })
 })
